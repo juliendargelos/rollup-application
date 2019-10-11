b6ac7e0eec1d2849ce1e313a50e00679622841a3
Julien Dargelos <julien.dargelos@me.com>
2019-10-11 11:18:16 +0200

    Fix condition exposing application instance in development environment

--- src/javascripts/index.ts
+++ src/javascripts/index.ts
@@ -4,4 +4,4 @@ import { Application } from './application'
 const application = new Application()
 application.mount(document.querySelector('.application')!)
 
-if (production) (global as any).application = application
+if (!production) (global as any).application = application
